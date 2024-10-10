ee0a7e6abb6efe8a5151433ea4c9b4908ef22a8f
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-10-10 15:23:44 +0100

    update task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,18 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
-  for (let value of array) {
+  for (const value of array) {
     array[idx] = appendString + value;
     idx++;
   }
 
   return array;
 }
+
+// export default function appendToEachArrayValue(array, appendString) {
+//     for (var idx in array) {
+//       var value = array[idx];
+//       array[idx] = appendString + value;
+//     }
+
+//     return array;
+//   }
