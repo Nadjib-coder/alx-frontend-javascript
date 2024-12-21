176f119c4f9ec181f88104ccdd69821cd1ddf10c
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-21 12:44:45 +0000

    update task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   uploadPhoto.then(() => {
     console.log('body ');
   });
