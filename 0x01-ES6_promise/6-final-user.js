81e9c4f2c06898ccc45a594c50d999f0226ae02a
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-21 20:03:05 +0000

    complet task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,32 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+// import { signUpUser } from './4-user-promise';
+// import { uploadPhoto } from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([
+//     signUpUser(firstName, lastName),
+//     uploadPhoto(fileName),
+//   ]).then((res) =>
+//     res.map((x) => ({
+//       status: x.status,
+//       value: x.status === 'fulfilled' ? x.value : String(x.reason),
+//     })),
+//   );
+// }
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((res) =>
-    res.map((x) => ({
-      status: x.status,
-      value: x.status === 'fulfilled' ? x.value : String(x.reason),
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === 'fulfilled' ? o.value : String(o.reason),
     })),
   );
 }
