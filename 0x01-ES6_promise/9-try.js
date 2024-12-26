2656f57d6336b540ae68c9edf55c171c3e612fcf
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-26 21:27:33 +0000

    complet task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,9 @@
+export default function guardrail(mathFunction) {
+  let queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error);
+  }
+  queue.push('Guardrail was processed');
+}
