ec3ef2950d7593cf66a679e1db42896d499900aa
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-28 21:54:29 +0000

    remove _ from the attribute in the constructor in task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,95 +1,43 @@
-// export default class ALXCourse {
-//   constructor(name, length, students) {
-//     this._name = name;
-//     this._length = length;
-//     this._students = students;
-//   }
-
-//   get name() {
-//     return this._name;
-//   }
-
-//   set name(newName) {
-//     if (typeof newName === 'string') {
-//       this._name = newName;
-//     } else {
-//       throw new TypeError('Name must be a string');
-//     }
-//   }
-
-//   get length() {
-//     return this._length;
-//   }
-
-//   set length(newLength) {
-//     if (typeof newLength === 'number') {
-//       this._length = newLength;
-//     } else {
-//       throw new TypeError('Length must be a number');
-//     }
-//   }
-
-//   get students() {
-//     return this._students;
-//   }
-
-//   set students(newStudents) {
-//     if (newStudents instanceof Array) {
-//       this._students = newStudents;
-//     } else {
-//       throw new TypeError('Students must be an array');
-//     }
-//   }
-// }
-class HolbertonCourse {
+export default class ALXCourse {
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
     this.students = students;
   }
 
-  /**
-   * @param {String} name
-   */
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
-  }
-
   get name() {
     return this._name;
   }
 
-  /**
-   * @param {Number} length
-   */
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
-    this._length = length;
   }
 
   get length() {
     return this._length;
   }
 
-  /**
-   * @param {Array} students
-   */
-  set students(students) {
-    if (students instanceof Array) {
-      this._students = students;
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
     } else {
-      throw new TypeError('Students must be an Array');
+      throw new TypeError('Length must be a number');
     }
   }
 
   get students() {
     return this._students;
   }
-}
 
-export default HolbertonCourse;
+  set students(newStudents) {
+    if (newStudents instanceof Array) {
+      this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+}
