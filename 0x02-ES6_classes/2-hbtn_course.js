ad94739000e4cc885b4f884ce2942f2f5a4aa54b
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-28 21:38:37 +0000

    complet task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class ALXCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (newStudents instanceof Array) {
+      this._students = newStudents;
+    }
+  }
+}
