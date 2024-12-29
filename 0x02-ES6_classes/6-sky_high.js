b33cfd40ef11115482622ae4ab3552d30d4416c2
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-29 23:57:35 +0000

    complet task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,4 +1,32 @@
-import Building from '5-building';
+// import Building from '5-building';
+
+// export default class SkyHighBuilding extends Building {
+//   constructor(sqft, floors) {
+//     super(sqft);
+//     this.floors = floors;
+//   }
+
+//   get floors() {
+//     return this._floors;
+//   }
+
+//   set floors(newFloors) {
+//     if (typeof newFloors === 'number') {
+//       this._floors = newFloors;
+//     } else {
+//       throw new TypeError('Floors must be a number');
+//     }
+//   }
+
+//   get sqft() {
+//     return this._sqft;
+//   }
+
+//   evacuationWarningMessage() {
+//     return `Evacuate slowly the ${this.floors} floors`;
+//   }
+// }
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
@@ -10,16 +38,8 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors === 'number') {
-      this._floors = newFloors;
-    } else {
-      throw new TypeError('Floors must be a number');
-    }
-  }
-
-  get sqft() {
-    return this._sqft;
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
