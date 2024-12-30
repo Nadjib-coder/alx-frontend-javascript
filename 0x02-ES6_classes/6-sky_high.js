e2a215f8b440bf6fa7d65d98f1db89509df14b7b
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-30 00:10:32 +0000

    complet task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -11,15 +11,7 @@ export default class SkyHighBuilding extends Building {
   }
 
   set floors(newFloors) {
-    if (typeof newFloors === 'number') {
-      this._floors = newFloors;
-    } else {
-      throw new TypeError('Floors must be a number');
-    }
-  }
-
-  get sqft() {
-    return this._sqft;
+    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
