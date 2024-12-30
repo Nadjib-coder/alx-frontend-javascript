0861bcc6c94d889b9a5c43d466511414f3dd74e0
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-30 00:30:01 +0000

    complet task 7

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+import { escape } from 'querystring';
+
+export default class ALXClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } else if (hint === 'string') {
+      return this._location;
+    }
+  }
+}
